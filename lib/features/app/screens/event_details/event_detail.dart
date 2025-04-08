9426202bd40d2a46683dcf9e5f3466faea7d0145
LuisangelParra <lfaria@uninorte.edu.co>
2025-04-07 21:17:24 -0500

    event detail screen

--- lib/features/app/screens/event_details/event_detail.dart
+++ lib/features/app/screens/event_details/event_detail.dart
@@ -27,6 +27,8 @@ class EventDetailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: dark ? LColors.dark.withValues(alpha: 0.95) : LColors.light.withValues(alpha: 0.95),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             children:
@@ -37,30 +39,118 @@ class EventDetailScreen extends StatelessWidget {
                 height: 325,
                 child: Column(),
               ),
-              LAppBar(
-                showBackArrow: true,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: LSizes.lg),
+                child: LAppBar(
+                  showBackArrow: true,
+                ),
               ),
             ],
           ),
           SizedBox(height: LSizes.spaceBtwSections/2),
-
-              // Popular events list
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: LSizes.lg * 1.5),
-                child: Column(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: LSizes.lg * 1.5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        LSectionHeading(
+                          title: event.title,
+                          isDetailTitle: true,
+                          textColor: dark ? LColors.textWhite : LColors.dark,
+                          showActionButton: false,
+                        ),
+                      ],
+                ),
+                SizedBox(height: LSizes.sm),
+                // Date and location
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    LSectionHeading(
-                      title: event.title,
-                      isDetailTitle: true,
-                      textColor: dark ? LColors.textWhite : LColors.dark,
-                      showActionButton: false,
+                    // Location chip it could be online or in an especific room
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: LSizes.sm, horizontal: LSizes.md),
+                      decoration: BoxDecoration(
+                        color: dark ? LColors.textWhite : LColors.dark,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        event.location,
+                        style: TextStyle(
+                          color: dark ? LColors.dark : LColors.textWhite,
+                          fontSize: LSizes.fontSizeSm,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: LSizes.sm),
+                    Text(
+                      // Format the date to a readable format WeekDay, Month Day, Year
+                      LHelperFunctions.formatDate(event.date),
+                      style: TextStyle(
+                        color: dark ? LColors.textWhite : LColors.dark,
+                        fontSize: LSizes.fontSizeSm,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(width: LSizes.sm),
+                    // Time of the event
+                    Text(
+                      // Format the time to a readable format Hour:Minute AM/PM
+                      // Example: 10:30 AM
+                      '${event.date.hour > 12 ? event.date.hour - 12 : event.date.hour}:${event.date.minute.toString().padLeft(2, '0')} ${event.date.hour >= 12 ? 'PM' : 'AM'}',
+                      style: TextStyle(
+                        color: dark ? LColors.textWhite : LColors.dark,
+                        fontSize: LSizes.fontSizeSm,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: LSizes.spaceBtwSections/2),
+                SizedBox(height: LSizes.spaceBtwSections/2),
+                          // Event description
+                Text(
+                  event.description,
+                  style: TextStyle(
+                    color: dark ? LColors.textWhite : LColors.dark,
+                    fontSize: LSizes.fontSizeMd,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+
+                SizedBox(height: LSizes.spaceBtwSections),
+                // Subscribe button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle subscribe action
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: dark ? LColors.textWhite : LColors.dark,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: LSizes.sm, horizontal: LSizes.md),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      'Subscribe',
+                      style: TextStyle(
+                        fontSize: LSizes.fontSizeMd,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
         ],
       ),
     );
