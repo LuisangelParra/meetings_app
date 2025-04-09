fee0cd14793b1dd47f61e59a9155b0c53db58230
LuisangelParra <lfaria@uninorte.edu.co>
2025-04-08 21:44:47 -0500

    Implementa pantalla de calendario con gestión de eventos; añade CalendarController y refactoriza main.dart para soporte de Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:meetings_app/features/app/controllers/calendar_controller.dart';
 import 'app.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async{
   await initializeDateFormatting('es_ES', null);
-  runApp(const App());
+  runApp(    
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CalendarController()),
+        // Otros providers...
+      ],
+      child: const App(),
+    ),
+  );
 }
